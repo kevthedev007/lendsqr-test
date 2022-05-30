5c79164c019ef96f5fd507a24c421eca58ec0f04
Kadiri Kelvin <kevthedev007@gmail.com>
2022-05-30 02:55:54 +0100

    Update 20220528123950_create_users_table.js

--- migrations/20220528123950_create_users_table.js
+++ migrations/20220528123950_create_users_table.js
@@ -12,22 +12,22 @@ exports.up = function (knex) {
     table.string("password", 1000).notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now())
   })
-    .createTable("accounts", table => {
-      // table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
-      table.increments("id").primary();
-      table.string("accountName", 255).notNullable();
-      // table.string("bankName").notNullable();
-      table.string("accountNumber").notNullable();
-      table.double("balance").notNullable().defaultTo(0);
-      table.integer("userId").unique().references("id").inTable("users").onDelete("cascade");
-    })
-    .createTable("transactions", table => {
-      table.increments("id").primary();
-      table.enum('transactionType', ['withdrawal', 'transfer', 'deposit']).notNullable();
-      table.double("amount").notNullable();
-      table.enum("status", ["success", "failed"]).defaultTo("success").notNullable();
-      table.integer("userId").references("id").inTable("users").onDelete("cascade");
-    })
+  // .createTable("accounts", table => {
+  //   // table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
+  //   table.increments("id").primary();
+  //   table.string("accountName", 255).notNullable();
+  //   // table.string("bankName").notNullable();
+  //   table.string("accountNumber").notNullable();
+  //   table.double("balance").notNullable().defaultTo(0);
+  //   table.integer("userId").unique().references("id").inTable("users").onDelete("cascade");
+  // })
+  // .createTable("transactions", table => {
+  //   table.increments("id").primary();
+  //   table.enum('transactionType', ['withdrawal', 'transfer', 'deposit']).notNullable();
+  //   table.double("amount").notNullable();
+  //   table.enum("status", ["success", "failed"]).defaultTo("success").notNullable();
+  //   table.integer("userId").references("id").inTable("users").onDelete("cascade");
+  // })
 };
 
 /**
