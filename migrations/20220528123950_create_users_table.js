8e1a5a3a7af66f7dd3c7e5c36be936f3612361e2
Kadiri Kelvin <kevthedev007@gmail.com>
2022-05-30 02:22:40 +0100

    Update 20220528123950_create_users_table.js

--- migrations/20220528123950_create_users_table.js
+++ migrations/20220528123950_create_users_table.js
@@ -2,8 +2,8 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-  return knex.schema.createTable("users", table => {
+exports.up = async function (knex, promise) {
+  await knex.schema.createTable("users", table => {
     // table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
     table.increments("id").primary();
     table.string("firstName", 255).notNullable();
@@ -12,22 +12,24 @@ exports.up = function (knex) {
     table.string("password", 1000).notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now())
   })
-    .createTable("accounts", table => {
-      // table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
-      table.increments("id").primary();
-      table.string("accountName", 255).notNullable();
-      // table.string("bankName").notNullable();
-      table.string("accountNumber").notNullable();
-      table.double("balance").notNullable().defaultTo(0);
-      table.foreign('userId').unsigned().references('users.id');
-    })
-    .createTable("transactions", table => {
-      table.increments("id").primary();
-      table.enum('transactionType', ['withdrawal', 'transfer', 'deposit']).notNullable();
-      table.double("amount").notNullable();
-      table.enum("status", ["success", "failed"]).defaultTo("success").notNullable();
-      table.foreign('userId').unsigned().references('users.id');
-    })
+
+  await knex.schema.createTable("accounts", table => {
+    // table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
+    table.increments("id").primary();
+    table.string("accountName", 255).notNullable();
+    // table.string("bankName").notNullable();
+    table.string("accountNumber").notNullable();
+    table.double("balance").notNullable().defaultTo(0);
+    table.foreign('userId').references('users.id');
+  })
+
+  await knex.schema.createTable("transactions", table => {
+    table.increments("id").primary();
+    table.enum('transactionType', ['withdrawal', 'transfer', 'deposit']).notNullable();
+    table.double("amount").notNullable();
+    table.enum("status", ["success", "failed"]).defaultTo("success").notNullable();
+    table.foreign('userId').references('users.id');
+  })
 };
 
 /**
