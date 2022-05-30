997f4d0797ba11767bb6e66c62648f83f2d8e104
Kadiri Kelvin <kevthedev007@gmail.com>
2022-05-30 04:22:42 +0100

    Update account.route.js

--- routes/account.route.js
+++ routes/account.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { fundAccount, transferFund, withdraw } = require("../controllers/account.controller")
+const { fundAccount, transferFund, withdraw, checkBalance } = require("../controllers/account.controller")
 const { verifyToken } = require("../util/verify")
 const { depositValidation, transferValidation } = require("../validations/account/account.validation")
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 router.patch("/deposit", [depositValidation, verifyToken], fundAccount);
 router.patch("/transfer", [transferValidation, verifyToken], transferFund);
 router.patch("/withdraw", [depositValidation, verifyToken], withdraw);
-router.get("/balance", verifyToken, withdraw);
+router.get("/balance", verifyToken, checkBalance);
 
 module.exports = router
